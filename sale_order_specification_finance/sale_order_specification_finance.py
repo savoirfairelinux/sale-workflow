45efaff13d1af8b2d02cc8a86c67cbd84685edad
Jordi Riera <jordi.riera@savoirfairelinux.com>
2014-08-28 09:13:54 -0400

    [#53878] sale_order_specification_finance: Removed old reference to an attempt to validate the url.

--- sale_order_specification_finance/sale_order_specification_finance.py
+++ sale_order_specification_finance/sale_order_specification_finance.py
@@ -5,8 +5,8 @@
 # (<http://www.savoirfairelinux.com>).
 #
 # This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,7 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import re
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -26,21 +25,11 @@ logging.basicConfig()
 # _logger.setLevel(logging.DEBUG)
 
 from openerp.osv import fields, orm
-from openerp.tools.translate import _
-
-url_regex = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  #domain...
-    r'localhost|'  #localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
 class sale_order_specification_finance(orm.Model):
     """ Add the notion of revision to sale.order """
     _inherit = 'sale.order'
-
 
     _columns = {
         'credit_form': fields.selection(
