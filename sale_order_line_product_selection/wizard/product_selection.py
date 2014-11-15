c25e7aebf793eda9cdac4d7a80e5a0e64c690e63
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2014-11-14 19:03:47 -0500

    [ADD] Module sale_order_line_product_selection initial version

---
+++ sale_order_line_product_selection/wizard/product_selection.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Savoir-faire Linux (<www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+
+
+class product_selection(models.TransientModel):
+
+    _name = 'product.selection'
+
+    partner_id = fields.Many2one('res.partner', string="Partner")
+    pricelist_id = fields.Many2one('product.pricelist', string="Pricelist")
+
+    brand_id = fields.Many2one('fleet.vehicle.model.brand', string='Brand')
+    model_id = fields.Many2one('fleet.vehicle.model', string='Model')
+    year = fields.Char('Year', size=12)
+    cylinder_id = fields.Many2one('fleet.vehicle.cylinder', string='Cylinders')
+    litre_id = fields.Many2one('fleet.vehicle.litre', string='Litre')
+
+    engine_code = fields.Char(string='Engine Code', size=154)
+    default_code = fields.Char(string='Internal Reference')
+
+    a_t = fields.Boolean('A/T')
+    m_t = fields.Boolean('M/T')
+
+    direct_fit_radiator_side_precat_left_side = fields.Boolean(
+        'Direct fit radiator side precat LEFT side')
+    direct_fit_radiator_side_precat_right_side = fields.Boolean(
+        'Direct fit radiator side precat RIGHT side')
+
+    dohc = fields.Boolean('DOHC')
+    shoc = fields.Boolean('SHOC')
+
+    converter_wmanifold = fields.Boolean('Converter W/Manifold')
+
+    super_duty = fields.Boolean('Super Duty')
+    b2x4 = fields.Boolean('2X4')
+    b4x4 = fields.Boolean('4X4')
+
+    awd = fields.Boolean('AWD')
+    fwd = fields.Boolean('FWD')
+
+    b2doors = fields.Boolean('2 doors')
+    b4doors = fields.Boolean('4 doors')
+
+    emission_federal = fields.Boolean('Emission Federal')
+    emission_californien = fields.Boolean('Emission Californien')
+
+    product_selection_line_ids = fields.One2many(
+        'product.selection.line',
+        'product_selection_id',
+        string='Search Result'
+    )
+
+    @api.multi
+    def update(self):
+        self.write({'product_selection_line_ids': [(5, 0, 0)]})
+
+        query = []
+        if (self.model_id or self.brand_id or self.year or self.cylinder_id or
+                self.litre_id):
+
+            query0 = []
+            if self.model_id:
+                query0.append(('model_id', '=', self.model_id.id))
+
+            if self.brand_id:
+                query0.append(('brand_id', '=', self.brand_id.id))
+
+            if self.year:
+                query0.append(('year', '=', self.year))
+
+            if self.cylinder_id:
+                query0.append(('cylinder_id', '=', self.cylinder_id.id))
+
+            if self.litre_id:
+                query0.append(('litre_id', '=', self.litre_id.id))
+
+            stage_1_ids = self.env['product.fleet.spec'].search(query0)
+            query.append(
+                ('product_fleet_spec_ids.id', 'in', stage_1_ids.ids))
+
+        if self.engine_code:
+            query.append(('engine_code', 'ilike', self.engine_code))
+
+        if self.default_code:
+            query.append(('default_code', 'ilike', self.default_code))
+
+        if self.a_t:
+            query.append(('a_t', '=', self.a_t))
+
+        if self.m_t:
+            query.append(('m_t', '=', self.m_t))
+
+        if (self.direct_fit_radiator_side_precat_left_side or
+                self.direct_fit_radiator_side_precat_right_side):
+            query.append(
+                ('direct_fit_radiator_side_precat_right_side', '=',
+                 self.direct_fit_radiator_side_precat_right_side)
+            )
+            query.append((
+                'direct_fit_radiator_side_precat_right_side', '=',
+                self.direct_fit_radiator_side_precat_right_side)
+            )
+
+        if self.dohc:
+            query.append(('dohc', '=', self.dohc))
+        if self.shoc:
+            query.append(('shoc', '=', self.shoc))
+
+        if self.converter_wmanifold:
+            query.append(
+                ('converter_wmanifold', '=', self.converter_wmanifold))
+
+        if self.super_duty:
+            query.append(('super_duty', '=', self.super_duty))
+        if self.b2x4:
+            query.append(('b2x4', '=', self.b2x4))
+        if self.b4x4:
+            query.append(('b4x4', '=', self.b4x4))
+
+        if self.awd:
+            query.append(('awd', '=', self.awd))
+        if self.fwd:
+            query.append(('fwd', '=', self.fwd))
+
+        if self.b2doors:
+            query.append(('b2doors', '=', self.b2doors))
+        if self.b4doors:
+            query.append(('b4doors', '=', self.b4doors))
+
+        if self.emission_federal:
+            query.append(('emission_federal', '=', self.emission_federal))
+        if self.emission_californien:
+            query.append(
+                ('emission_californien', '=', self.emission_californien)
+            )
+
+        product_ids = self.env['product.template'].search(query)
+
+        if product_ids:
+            vals = {
+                'product_selection_line_ids': [
+                    (0, 0, {'product_id': p}) for p in product_ids.ids]
+            }
+            self.write(vals)
+
+        return {
+            'name': _('Select products from the result list'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'product.selection',
+            'res_id': self.ids[0],
+            'target': 'new',
+            'context': self.env.context,
+        }
+
+    @api.one
+    def confirm(self):
+        vals = [
+            (0, 0, {
+                'product_id': line.product_id.id,
+                'price_unit': line.product_id.list_price,
+            })
+            for line in self.product_selection_line_ids
+            if line.is_selected
+        ]
+        so = self.env['sale.order'].browse(self._context.get('active_id'))
+        so.write({'order_line': vals})
+
+        return {'type': 'ir.actions.act_window_close'}
---
+++ sale_order_line_product_selection/wizard/product_selection_line.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Savoir-faire Linux (<www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
+import time
+
+
+class product_selection_line(models.TransientModel):
+
+    _name = 'product.selection.line'
+
+    @api.one
+    @api.depends('product_id.list_price', 'pricelist_id')
+    def _list_price(self):
+        pricelist = self.env['product.pricelist'].browse(
+            [self.product_selection_id.pricelist_id.id])
+        self.list_price = pricelist.price_get(
+            self.product_id.id, 1.0,
+            partner=self.product_selection_id.partner_id.id)[pricelist.id]
+
+    product_selection_id = fields.Many2one('product.selection')
+    is_selected = fields.Boolean('Selected')
+    product_id = fields.Many2one('product.template', string='Product')
+    pricelist_id = fields.Many2one('product.pricelist')
+    default_code = fields.Char(related='product_id.default_code', store=True)
+    standard_price = fields.Float(
+        related='product_id.standard_price', store=True)
+    list_price = fields.Float(
+        compute=_list_price, string="Sale Price")
